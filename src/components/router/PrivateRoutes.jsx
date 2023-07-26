7d7c927f6d5d728f8c255d076aa8299fe866beba
hoang2807 <viethoang28072000@gmail.com>
2023-07-26 22:35:16 +0700

    update route

--- src/components/router/PrivateRoutes.jsx
+++ src/components/router/PrivateRoutes.jsx
@@ -1,1 +1,12 @@
-export default PrivateRoutes
+import { Route, Routes } from 'react-router-dom';
+import Home from '../../pages/Home/Home';
+
+const PrivateRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
+
+export default PrivateRoutes;
